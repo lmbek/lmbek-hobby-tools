74cfafe332cca0e7236e27555eccf7734aa182ff
Lars Morten Bek <lars@beksoft.dk>
2026-07-06 01:31:12 +0200

    refactor(deployer): remove unused deployer tool
    Deleted unused Terraform configurations and related documentation.

--- deployer/main.tf
+++
@@ -1,7 +0,0 @@
-provider "local" {
-}
-
-resource "local_file" "infra_definition" {
-  content  = "Infrastructure deployed via Terraform deployer tool."
-  filename = "${path.module}/../../infrastructure/terraform-status.txt"
-}
